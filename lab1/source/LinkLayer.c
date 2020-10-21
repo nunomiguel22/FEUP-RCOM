5e165ab10da4e824ba46ac4d90429106674aa07a
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2020-10-21 18:12:16 +0100

    Link layer and file restructure update

--- lab1/source/LinkLayer.c
+++ lab1/source/LinkLayer.c
@@ -14,13 +14,18 @@
 #define _POSIX_SOURCE 1
 
 /* Framing */
+#define CONTROL_FRAME_SIZE 5
 #define MAX_FRAME_SIZE 6
+#define FLAG 0x7E  // Flag for beggining and ending of frame
+#define ESC 0x7D   // Escape character for byte stuffing
+#define AF1 0x03   // Transmitter commands, Receiver replys
+#define AF2 0x01   // Transmitter replys, Receiver commands
 
 /* Serial Port */
 #define DEFAULT_BAUDRATE B38400
 #define MAX_PORT_LENGTH 20
 
-/*  Port Name */
+/*  Port name prefix */
 #ifdef __linux__
 #define PORT_NAME "/dev/ttyS"
 #elif _WIN32
@@ -37,16 +42,36 @@ typedef struct {
   unsigned int timeout;
   unsigned int numTransmissions;
   char frame[MAX_FRAME_SIZE];
-  bool init;
 } LinkLayer;
 
+typedef enum {
+  INF = 0x00,
+  SET = 0x03,
+  DISC = 0x0B,
+  UA = 0x07,
+  RR = 0x05,
+  REJ = 0x01
+} ControlTypes;
+
 /* Globals */
-LinkLayer ll;
+static LinkLayer ll;
+static LinkType linkType;
+
+/* Auxiliar Functions Declarations */
+int initSerialPort(int port, LinkType type);
+bool isControlCommand(ControlTypes type);
+void createControlFrame(char* frame, ControlTypes controlType);
+int sendControlFrame(int fd, ControlTypes controlType);
 
 /* Link Layer Functions */
-int llopen(int port, linkType type) {
-  int fd = initSerialPort(port);
+int llopen(int port, LinkType type) {
+  int fd = initSerialPort(port, type);
   if (fd == -1) return fd;
+
+  if (linkType == TRANSMITTER) {
+    sendControlFrame(fd, SET);
+  } else if (linkType == RECEIVER) {
+  }
 
   // TODO: SEND AND READ CONTROL FRAME DEPENDING ON TRANS/RECEIVER
 
@@ -58,10 +83,42 @@ int llwrite(int fd, char* buffer, int length);
 int llread(int fd, char* buffer);
 
 /* Auxiliar Functions */
-int initSerialPort(int port) {
+
+int sendControlFrame(int fd, ControlTypes controlType) {
+  char frame[CONTROL_FRAME_SIZE];
+  createControlFrame(frame, controlType);
+  return write(fd, frame, CONTROL_FRAME_SIZE);
+}
+
+void createControlFrame(char* frame, ControlTypes controlType) {
+  // Frame start flag
+  frame[0] = FLAG;
+
+  // Address Field
+  frame[1] = AF1;
+  if (linkType == RECEIVER && isControlCommand(controlType))
+    frame[1] = AF2;
+  else if (linkType == TRANSMITTER && !isControlCommand(controlType))
+    frame[1] = AF2;
+
+  // Control Field
+  frame[2] = controlType;
+  // BCC Field (Address Field ^ Control Field)
+  frame[3] = frame[1] ^ frame[2];
+  // Frame end flag
+  frame[4] = FLAG;
+}
+
+bool isControlCommand(ControlTypes type) {
+  if (type == INF || type == DISC || type == SET) return true;
+  return false;
+}
+
+int initSerialPort(int port, LinkType type) {
   // Init ll struct
   snprintf(ll.port, MAX_PORT_LENGTH, "%s%d", PORT_NAME, port);
   ll.baudRate = DEFAULT_BAUDRATE;
+  linkType = type;
 
   int fd = open(ll.port, O_RDWR | O_NOCTTY);
   if (fd < 0) {
--- lab1/source/LinkLayer.h
+++ lab1/source/LinkLayer.h
@@ -1,9 +1,9 @@
 #ifndef LINKLAYER_H
 #define LINKLAYER_H
 
-typedef enum { TRANSMITTER, RECEIVER } linkType;
+typedef enum { TRANSMITTER, RECEIVER } LinkType;
 
-int llopen(int port, linkType type);
+int llopen(int port, LinkType type);
 int llclose(int fd);
 int llwrite(int fd, char* buffer, int length);
 int llread(int fd, char* buffer);
