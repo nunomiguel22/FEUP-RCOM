72621659cb8a7a5d56c53d6e581923d81185ea51
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2020-11-09 19:02:24 +0000

    a

--- lab1/source/link_layer.c
+++ lab1/source/link_layer.c
@@ -177,8 +177,8 @@ int llclose(int fd) {
     char_buffer_destroy(&discFrame);
 
     // Send UA
-    usleep(5);
     send_control_frame(fd, LL_UA);
+    usleep(5);
     log_msg("llclose - disconnected.\n");
     close_serial_port(fd);
     return 1;
